98dab25c02fc0d8dad76f6ed86649b7e0d6fff09
George Oremo <george.oremo@gmail.com>
2020-08-21 16:01:48 +0300

    - set environment variables     file: <root>/ngx-admin/src/environments/environment.ts     new contents on environment variables:     apiEndpoint: 'http://localhost/cdapi/public/api/cd',     consumerToken: 'B0B3DA99-1859-A499-90F6-1E3F69575DCD',     consumer: null
    - removed dependancy for google-libphonenumber

--- src/app/@cd/sys/user/validators/phone.validator2.ts
+++ src/app/@cd/sys/user/validators/phone.validator2.ts
@@ -1,18 +1,18 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
-import { PhoneNumberUtil, PhoneNumber } from 'google-libphonenumber';
+// import { PhoneNumberUtil, PhoneNumber } from 'google-libphonenumber';
 
-const phoneNumberUtil = PhoneNumberUtil.getInstance();
+// const phoneNumberUtil = PhoneNumberUtil.getInstance();
 
-export function PhoneNumberValidator(regionCode: string): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } => {
-        let validNumber = false;
-        try {
-            const phoneNumber = phoneNumberUtil.parseAndKeepRawInput(
-                control.value, regionCode
-            );
-            validNumber = phoneNumberUtil.isValidNumber(phoneNumber);
-        } catch (e) { }
+export function PhoneNumberValidator(regionCode: string) {
+    // return (control: AbstractControl): { [key: string]: any } => {
+    //     let validNumber = false;
+    //     try {
+    //         const phoneNumber = phoneNumberUtil.parseAndKeepRawInput(
+    //             control.value, regionCode
+    //         );
+    //         validNumber = phoneNumberUtil.isValidNumber(phoneNumber);
+    //     } catch (e) { }
 
-        return validNumber ? null : { wrongNumber: { value: control.value } };
-    };
+    //     return validNumber ? null : { wrongNumber: { value: control.value } };
+    // };
 }
